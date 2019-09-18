5aad02e2264ad37d4a78375856c90c6cc6792a9c
ladayjawynn26 <55443422+ladayjawynn26@users.noreply.github.com>
2019-09-18 08:25:00 -0400

    Add files via upload

---
+++ 2019-09-18-classwork/2019-09-18-classwork/main.cpp
@@ -0,0 +1,12 @@
+//Ladayja Wynn
+//Intro to Computer Science
+//Period 1
+//Mr. Williams
+//Classwork 2019-09-18
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "What's poppin'" << std::endl;
+}
