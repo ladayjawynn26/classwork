9cd6ef106b0cdcc6f1c8ebaeff7f26aadf684379
ladayjawynn26 <55443422+ladayjawynn26@users.noreply.github.com>
2019-09-24 08:38:46 -0400

    Add files via upload

---
+++ 2019-09-24 classwork/2019-09-24 classwork/main.cpp
@@ -0,0 +1,23 @@
+// Ladayja Wynn
+// Intro to CS
+// Mr. Williams
+// Period 1
+// 2019-09-24 classwork
+
+#include <iostream>
+
+int main()
+{
+	int x = 7;
+	int y = 8;
+	float c = 6;
+	bool a = 3;
+	int f = 2;
+	int d = 1000;
+	float r = 100;
+	double g = 5;
+	int h = 100000;
+	char b = 1000000;
+	std::cout << sizeof(x) << std::endl;
+	
+}
