587939ba6ed25cc1f025b3d32188922003938b66
ladayjawynn26 <55443422+ladayjawynn26@users.noreply.github.com>
2019-10-03 08:38:47 -0400

    Add files via upload

---
+++ 2019-10-3 Classwork/2019-10-3 Classwork/main.cpp
@@ -0,0 +1,16 @@
+//Ladayja Wynn
+//Intro to CS
+//Period 1
+//Mr. Williams
+//2019-10-3-Classwork
+
+#include <iostream>
+using namespace std;
+
+ int main()
+ {
+	std::cout << "8";
+	int favoriteNumber;
+	cin >> favoriteNumber;
+	cout << "Cool," << favoriteNumber << "is my favorite number too!" << endl;
+}
